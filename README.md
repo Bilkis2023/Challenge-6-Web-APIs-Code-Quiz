e9eca83cda7fcc75c4e43a71157f1239dfa6aba4
bilkis naher <bilkis.naher@yahoo.co.uk>
2024-01-04 11:06:03 +0000

    correction of question's answer, add license, add credits

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 ![schreenShot](.starter/assets/Quiz Screenshot.jpg)
 
 
+
+## User Story:
+As a  coding boot camp student and junior web Developer, I want to take a timed quiz on JavaScript fundamentals that stores high scores so that I can gauge my progress compared to my peers.
+
 # Acceptance Criteria
 
    - A start button that when clicked a timer starts and the first question appears.
@@ -19,4 +23,18 @@
 
   - The quiz should end when all questions are answered or the timer reaches 0.
 
-  - When the game ends, it should display their score and give the user the ability to save their initials and their score
+  - When the game ends, it should display their score and give the user the ability to save their initials and their score.
+
+
+  ## License:<br>
+
+-👉 - This project is licensed under the MIT License.
+
+
+
+## Credits:<br>
+- https://bootcampspot.instructure.com/courses/5076<br>
+- https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/<br>
+- https://www.markdownguide.org/cheat-sheet<br>
+- https://chat.openai.com/
+
--- starter/assets/script.js
+++ starter/assets/script.js
@@ -16,27 +16,27 @@ let timeLeft = 60;
 const questions = [
 
     {
-        question: "Commonly used data type do not include ____________.",
+        question: "Commonly used data types do not include ____________.",
         answers: [
             { text: "String's", correct: false },
             { text: "Number's", correct: false },
-            { text: "Booleans", correct: true },
-            { text: "Alerts", correct: false },
+            { text: "Booleans", correct: false },
+            { text: "Alerts", correct: true },
         ]
     },
 
     {
-        question: "The condition in an if/ else statment enclosed with ____________.",
+        question: "The condition in an if/ else statement enclosed with ____________.",
         answers: [
-            { text: "Qutes", correct: false },
-            { text: "Crully brackts", correct: false },
+            { text: "Quotes", correct: false },
+            { text: "Cruly brackets", correct: false },
             { text: "Parenthesis", correct: true },
-            { text: "Squar brackts", correct: false },
+            { text: "Square brackets", correct: false },
         ]
     },
 
     {
-        question: "Arrays in JavaScript can be store____________.",
+        question: "Arrays in JavaScript can be stored____________.",
         answers: [
             { text: "Numbers and strings", correct: false },
             { text: "Other string", correct: false },
@@ -46,22 +46,22 @@ const questions = [
     },
 
     {
-        question: "String values must be enclosed within ____________  when being assigned to variable.",
+        question: "String values must be enclosed within ____________  when being assigned to a variable.",
         answers: [
-            { text: "Coma", correct: true },
-            { text: "Crully brackts", correct: false },
+            { text: "Coma", correct: false},
+            { text: "Cruly brackets", correct: false },
             { text: "Parenthesis", correct: false },
-            { text: "Qoutes", correct: false },
+            { text: "Quotes", correct:  true },
         ]
     },
 
     {
-        question: "Very useful tool use during development and debugging for printing content to the debugger is ____________ .",
+        question: "Very useful tool used during development and debugging for printing content to the debugger is ____________ .",
         answers: [
-            { text: "JavaScript", correct: true },
+            { text: "JavaScript", correct: false},
             { text: "Forloops", correct: false },
             { text: "Terminal/bash", correct: false },
-            { text: "console.log", correct: false },
+            { text: "console.log", correct: true  },
         ]
     },
 
@@ -111,7 +111,6 @@ function selectAnswer(answer) {
     if (answer.correct) {
         var message = document.getElementById("correct")
         message.textContent = "correct"
-
         score++;
     } else {
         var message = document.getElementById("correct")
@@ -142,8 +141,7 @@ function endQuiz() {
 }
 
 function saveScore() {
-    var highscore = score
-    localStorage.setitem("highScore", highscore)
+   
 
     const initials = initialsInput.value.trim();
     if (initials !== '') {
